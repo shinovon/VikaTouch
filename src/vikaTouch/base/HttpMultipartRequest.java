bb2de83239a4c4b7d8f6c7b1221e872027fd6c4d
Shinovon <43963888+shinovon@users.noreply.github.com>
2020-07-25 10:40:56 -0700

    Update HttpMultipartRequest.java

--- src/vikaTouch/base/HttpMultipartRequest.java
+++ src/vikaTouch/base/HttpMultipartRequest.java
@@ -9,35 +9,41 @@ import java.util.Hashtable;
 import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
 
-public class HttpMultipartRequest {
+public class HttpMultipartRequest
+{
 	
-	private byte[] aByteArray15 = null;
-	private String aString16 = null;
+	private byte[] byteArray = null;
+	private String url = null;
 
-	public HttpMultipartRequest(String var1, Hashtable var2, String var3, String var4, String var5, byte[] var6) {
-		this.aString16 = var1;
-		var1 = "----------V2ymHFg03ehbqgZCaKO6jy";
+	public HttpMultipartRequest(String url, Hashtable var2, String var3, String var4, String var5, byte[] data)
+	{
+		this.url = url;
+		String var1 = "----------V2ymHFg03ehbqgZCaKO6jy";
 		String var7 = getBoundaryMessage("----------V2ymHFg03ehbqgZCaKO6jy", var2, var3, var4, var5);
 		var1 = "\r\n--" + var1 + "--\r\n";
 		ByteArrayOutputStream var8;
 		try
 		{
-		(var8 = new ByteArrayOutputStream()).write(var7.getBytes());
-		var8.write(var6);
-		var8.write(var1.getBytes());
-		this.aByteArray15 = var8.toByteArray();
-		var8.close();
-		}catch(Exception e)
+			var8 = new ByteArrayOutputStream();
+			var8.write(var7.getBytes());
+			var8.write(data);
+			var8.write(var1.getBytes());
+			this.byteArray = var8.toByteArray();
+			var8.close();
+		}
+		catch (Exception e)
 		{
 			
 		}
 	}
 	
-	private static String getBoundaryMessage(String var0, Hashtable var1, String var2, String var3, String var4) {
+	private static String getBoundaryMessage(String var0, Hashtable var1, String var2, String var3, String var4)
+	{
 		StringBuffer var5 = (new StringBuffer("--")).append(var0).append("\r\n");
 		Enumeration var6 = var1.keys();
 
-		while (var6.hasMoreElements()) {
+		while (var6.hasMoreElements())
+		{
 			String var7 = (String) var6.nextElement();
 			String var8 = (String) var1.get(var7);
 			var5.append("Content-Disposition: form-data; name=\"").append(var7).append("\"\r\n\r\n").append(var8)
@@ -49,41 +55,52 @@ public class HttpMultipartRequest {
 		return var5.toString();
 	}
 
-	public final byte[] send() {
+	public final byte[] send()
+	{
 		HttpConnection var1 = null;
 		InputStream var2 = null;
 		ByteArrayOutputStream var3 = new ByteArrayOutputStream();
 		byte[] var4 = null;
 
 		try {
-			(var1 = (HttpConnection) Connector.open(this.aString16)).setRequestProperty("Content-Type",
+			(var1 = (HttpConnection) Connector.open(this.url)).setRequestProperty("Content-Type",
 					"multipart/form-data; boundary=----------V2ymHFg03ehbqgZCaKO6jy");
 			var1.setRequestMethod("POST");
 			OutputStream var5;
-			(var5 = var1.openOutputStream()).write(this.aByteArray15);
+			(var5 = var1.openOutputStream()).write(this.byteArray);
 			var5.close();
 			var2 = var1.openInputStream();
 
 			int var13;
-			while ((var13 = var2.read()) != -1) {
+			while ((var13 = var2.read()) != -1)
+			{
 				var3.write(var13);
 			}
 
 			var4 = var3.toByteArray();
-		} catch (Exception var11) {
-			var11.printStackTrace();
-		} finally {
-			try {
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
 				var3.close();
-				if (var2 != null) {
+				if (var2 != null)
+				{
 					var2.close();
 				}
 
-				if (var1 != null) {
+				if (var1 != null)
+				{
 					var1.close();
 				}
-			} catch (Exception var10) {
-				var10.printStackTrace();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
 			}
 
 		}
