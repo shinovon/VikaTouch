590dde1b8a1989bcfa12c5c9db8dcd85eae7a78a
Feodor0090_LL <79257300125@ya.ru>
2020-08-06 22:25:18 +0300

    Create InfoPopup.java

---
+++ src/ru/nnproject/vikaui/InfoPopup.java
@@ -0,0 +1,77 @@
+package ru.nnproject.vikaui;
+
+import javax.microedition.lcdui.Font;
+import javax.microedition.lcdui.Graphics;
+
+import vikatouch.screens.MainScreen;
+
+public class InfoPopup implements IPopup {
+	private String[] lines;
+	private Thread ok;
+	private int linesCount;
+	
+	public InfoPopup(String text, Thread onOk)
+	{
+		lines = TextBreaker.breakText(text, false, null, true, Math.min(DisplayUtils.width-20, 400)-20);
+		ok = onOk;
+		int i=0;
+		while(i<lines.length && lines[i]!=null)
+		{
+			i++;
+		}
+		linesCount = i;
+	}
+	
+	public void Draw(Graphics g) {
+		int width = Math.min(DisplayUtils.width-20, 300);
+		Font f = Font.getFont(0, 0, Font.SIZE_MEDIUM);
+		int h1 = f.getHeight();
+		int th = h1*4 + h1*linesCount;
+		int y = DisplayUtils.height/2 - th/2;
+		int x = DisplayUtils.width/2 - width/2;
+		String okT = "ОК";
+		
+		// drawing
+		ColorUtils.setcolor(g, ColorUtils.BACKGROUND);
+		g.fillRoundRect(x, y, width, th, 16, 16);
+		
+		ColorUtils.setcolor(g, ColorUtils.BUTTONCOLOR);
+		g.fillRoundRect(DisplayUtils.width/2-25, y+h1*(linesCount+1), 50, h1*2, 14, 14);
+		
+		g.setFont(f);
+		g.setStrokeStyle(Graphics.SOLID);
+		ColorUtils.setcolor(g, ColorUtils.TEXT);
+		g.drawRoundRect(x, y, width, th, 16, 16); // бордер
+		for(int i = 0; i < linesCount;i++)
+		{
+			if(lines[i]!=null) g.drawString(lines[i], DisplayUtils.width/2 - f.stringWidth(lines[i])/2, y+h1/2+h1*1, 0);
+		}
+		ColorUtils.setcolor(g, ColorUtils.BACKGROUND);
+		g.drawString(okT, DisplayUtils.width/2-f.stringWidth(okT)/2, y+h1*(linesCount+1)+h1/2, 0);
+	}
+	
+	public void OnKey(int key)
+	{
+		MainScreen.activePopup = null;
+		if(ok!=null)
+			ok.start();
+	}
+	
+	public void OnTap(int x, int y)
+	{
+		int width = Math.min(DisplayUtils.width-20, 300);
+		Font f = Font.getFont(0, 0, Font.SIZE_MEDIUM);
+		int h1 = f.getHeight();
+		int th = h1*4 + h1*linesCount;
+		int ry = DisplayUtils.height/2 - th/2;
+		int rx = DisplayUtils.width/2 - width/2;
+		
+		if(y>ry+h1*(linesCount+1) && y<ry+th-h1)
+		{
+			if(x>DisplayUtils.width/2-25 && x<DisplayUtils.width/2+25)
+			{
+				OnKey(PressableUIItem.KEY_OK);
+			}
+		}
+	}
+}
