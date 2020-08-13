742323843e6f39297042d28db1d8910fc5d8f751
Shinovon <43963888+shinovon@users.noreply.github.com>
2020-08-14 00:36:09 +0500

    Update oauthproxy.php

--- server/oauthproxy.php
+++ server/oauthproxy.php
@@ -1,37 +1,37 @@
 <?php
 if(isset($_GET["url"]))
 {
-$url = $_GET["url"];
-$filename = "serverdata\page_" . randoms() . ".txt";
-$confidential = false;
-if(isset($_GET["token"]))
-{
-	$confidential = true;
-	$username = urlencode($_GET["user"]);
-	$password = urlencode($_GET["pass"]);
-	$url = "http://vk-oauth-proxy.xtrafrancyz.net:80/token?grant_type=password&client_id=2685278&client_secret=lxhD8OD7dMsqtXIm5IUY&username=".
-	$username.
-	"&password=".
-	$password.
-	"&scope=notify%2Cfriends%2Cphotos%2Caudio%2Cvideo%2Cdocs%2Cnotes%2Cpages%2Cstatus%2Coffers%2Cquestions%2Cwall%2Cgroups%2Cmessages%2Cnotifications%2Cstats%2Cads%2Coffline";
-	$filename = "serverdata\token_" . randoms() . ".txt";
-}
-$fp = fopen($filename, "w");
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_FILE, $fp);
-if(isset($_GET["kate"]))
-{
-	curl_setopt($ch, CURLOPT_USERAGENT, "KateMobileAndroid/51.1 lite-442 (Symbian; SDK 17; x86; Nokia; ru)");
-}
-curl_exec($ch);
-if(curl_error($ch))
-{
-    fwrite($fp, curl_error($ch));
-}
-curl_close($ch);
-fclose($fp);
-echo file_get_contents($filename);
-unlink($filename);
+	$url = $_GET["url"];
+	$filename = "serverdata\page_" . randoms() . ".txt";
+	$confidential = false;
+	if(isset($_GET["token"]))
+	{
+		$confidential = true;
+		$username = urlencode($_GET["user"]);
+		$password = urlencode($_GET["pass"]);
+		$url = "http://vk-oauth-proxy.xtrafrancyz.net:80/token?grant_type=password&client_id=2685278&client_secret=lxhD8OD7dMsqtXIm5IUY&username=".
+		$username.
+		"&password=".
+		$password.
+		"&scope=notify%2Cfriends%2Cphotos%2Caudio%2Cvideo%2Cdocs%2Cnotes%2Cpages%2Cstatus%2Coffers%2Cquestions%2Cwall%2Cgroups%2Cmessages%2Cnotifications%2Cstats%2Cads%2Coffline";
+		$filename = "serverdata\token_" . randoms() . ".txt";
+	}
+	$fp = fopen($filename, "w");
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_FILE, $fp);
+	if(isset($_GET["kate"]))
+	{
+		curl_setopt($ch, CURLOPT_USERAGENT, "KateMobileAndroid/51.1 lite-442 (Symbian; SDK 17; x86; Nokia; ru)");
+	}
+	curl_exec($ch);
+	if(curl_error($ch))
+	{
+		fwrite($fp, curl_error($ch));
+	}
+	curl_close($ch);
+	fclose($fp);
+	echo file_get_contents($filename);
+	unlink($filename);
 }
 else
 {
