69e3d85d5ebec618473bc8e59682ee839ae08d07
Feodor0090_LL <79257300125@ya.ru>
2020-09-08 00:01:22 +0300

    VEngine utils

--- src/ru/nnproject/vikaui/utils/text/TextBreaker.java
+++ src/ru/nnproject/vikaui/utils/text/TextBreaker.java
@@ -1,5 +1,7 @@
 package ru.nnproject.vikaui.utils.text;
 
+import java.util.Vector;
+
 import javax.microedition.lcdui.Font;
 
 import ru.nnproject.vikaui.menu.items.UIItem;
@@ -80,4 +82,62 @@ public class TextBreaker
 		return result;
 	}
 
+	// взято из виэна
+	public static String[] breakText(String text, Font font, int maxWidth)
+	{
+		if(text == null || text.length() == 0 || (text.length()==1&&text.charAt(0)==' '))
+		{
+			return new String[0];
+		}
+		Vector v = new Vector(5,3);
+		char[] chars = text.toCharArray();
+		if(font.stringWidth(text) > maxWidth)
+		{
+			int i1 = 0;
+			for(int i2 = 0; i2 < text.length(); i2++)
+			{
+				if(chars[i2] == '\n')
+				{
+					v.addElement(text.substring(i1, i2));
+					i2 = i1 = i2+1;
+				}
+				else
+				{
+					if(text.length() - i2 <= 1)
+					{
+						v.addElement(text.substring(i1, i2));
+						break;
+					}
+					else if(font.substringWidth(text, i1, i2-i1) >= maxWidth)
+					{
+						boolean f = false;
+						for(int j=i2; j>i1; j--)
+						{
+							char c = text.charAt(j);
+							if(c==' '||c=='-')
+							{
+								f = true;
+								v.addElement(text.substring(i1, j+1));
+								i2 = i1 = j+1;
+								break;
+							}
+						}
+						if(!f)
+						{
+							i2 = i2-2;
+							v.addElement(text.substring(i1, i2));
+							i2 = i1 = i2+1;
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			v.addElement(text);
+		}
+		String[] r = new String[v.size()];
+		v.copyInto(r);
+		return r;
+	}
 }
